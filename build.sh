900f92916cdb40245e27705d6d03bd8bcdfbdd5b
Everton Andrade <everton.aulian@gmail.com>
2019-09-02 15:39:58 -0300

    Splitting into 4 different servers

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #export AWS_ACCESS_KEY_ID=
 #export AWS_SECRET_ACCESS_KEY=
 
-ip=`curl -s https://api.ipify.org`
+IP=`curl -s https://api.ipify.org`
+key=`cat ~/.ssh/everton.aulianNV.pem`
 
-ansible-playbook -v provisioning.yml -i hosts --extra-vars "PUBLICIP=$ip USERPASSWORD=$1"
+ansible-playbook -v provisioning.yml -i hosts --extra-vars "PUBLICIP=$IP KEY=\"$key\""
